b21ff55c8e6e3ed271ef69538aa3db2080f823f7
DarioBarrionuevo <dariobafran@gmail.com>
2020-09-30 21:43:01 +0200

    Añadido Europa y All. Signos de puntuación

--- covidCounter.js
+++ covidCounter.js
@@ -1,18 +1,22 @@
-const div = document.querySelector("#contador");
+// Contador España
 
-function cogerDatosCOVID() {
+const divSpain = document.querySelector("#contadorSpain");
+
+function cogerDatosSpain() {
   fetch("https://disease.sh/v3/covid-19/countries/spain")
     .then((response) => response.json()) //Lo paso de http a json
     .then((datos) => {
       const data = datos;
 
-      div.innerHTML = `
+      // number.toLocaleString('de-DE', { style: 'decimal', decimal: '3' }) para que ponga los puntos
+
+      divSpain.innerHTML = `
             
-            <h3>Casos totales: ${data.cases}</h3>
-            <h3>Muertes totales: ${data.deaths}</h3>
-            <h3>Casos hoy: ${data.todayCases}</h3>
-            <h3>Muertes hoy: ${data.todayDeaths}</h3>
-            <h3>Estado critico: ${data.critical}</h3>
+            <h3>Casos totales: ${data.cases.toLocaleString("de-DE")}</h3>
+            <h3>Muertes totales: ${data.deaths.toLocaleString("de-DE")}</h3>
+            <h3>Casos hoy: ${data.todayCases.toLocaleString("de-DE")}</h3>
+            <h3>Muertes hoy: ${data.todayDeaths.toLocaleString("de-DE")}</h3>
+            <h3>Estado critico: ${data.critical.toLocaleString("de-DE")}</h3>
                     `;
     })
     .catch((error) => {
@@ -20,8 +24,72 @@ function cogerDatosCOVID() {
     });
 }
 
-cogerDatosCOVID();
+cogerDatosSpain();
 
 setInterval(() => {
-  cogerDatosCOVID();
-}, 600000);
+  cogerDatosSpain();
+}, 43200000); //12 horas en milisegundos
+
+// Contador Europa
+
+const divEurope = document.querySelector("#contadorEurope");
+
+function cogerDatosEurope() {
+  fetch("https://disease.sh/v3/covid-19/continents/europe")
+    .then((response) => response.json()) //Lo paso de http a json
+    .then((datos) => {
+      const data = datos;
+
+      // number.toLocaleString('de-DE', { style: 'decimal', decimal: '3' }) para que ponga los puntos
+
+      divEurope.innerHTML = `
+            
+            <h3>Casos totales: ${data.cases.toLocaleString("de-DE")}</h3>
+            <h3>Muertes totales: ${data.deaths.toLocaleString("de-DE")}</h3>
+            <h3>Casos hoy: ${data.todayCases.toLocaleString("de-DE")}</h3>
+            <h3>Muertes hoy: ${data.todayDeaths.toLocaleString("de-DE")}</h3>
+            <h3>Estado critico: ${data.critical.toLocaleString("de-DE")}</h3>
+                    `;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+cogerDatosEurope();
+
+setInterval(() => {
+  cogerDatosEurope();
+}, 600000); //10 minutos en milisegundos
+
+// Contador Mundo
+
+const divAll = document.querySelector("#contadorAll");
+
+function cogerDatosAll() {
+  fetch("https://disease.sh/v3/covid-19/all")
+    .then((response) => response.json()) //Lo paso de http a json
+    .then((datos) => {
+      const data = datos;
+
+      // number.toLocaleString('de-DE', { style: 'decimal', decimal: '3' }) para que ponga los puntos
+
+      divAll.innerHTML = `
+            
+            <h3>Casos totales: ${data.cases.toLocaleString("de-DE")}</h3>
+            <h3>Muertes totales: ${data.deaths.toLocaleString("de-DE")}</h3>
+            <h3>Casos hoy: ${data.todayCases.toLocaleString("de-DE")}</h3>
+            <h3>Muertes hoy: ${data.todayDeaths.toLocaleString("de-DE")}</h3>
+            <h3>Estado critico: ${data.critical.toLocaleString("de-DE")}</h3>
+                    `;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+cogerDatosAll();
+
+setInterval(() => {
+  cogerDatosAll();
+}, 600000); //10 minutos en milisegundos
